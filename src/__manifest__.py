7877958f6a433b259953190640ad296e7b6ffafe
ferdinandhubbard981 <ferdinandhubbard@gmail.com>
2024-10-23 09:39:39 +0100

    added basic views, some fields the installation model & other boilerplate content

---
+++ src/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'spqm',
+    'description': 'generate quotes for a solar panel system installation',
+    'depends': ['base'],
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/spqm_installation_views.xml',
+        'views/spqm_menus.xml'
+    ],
+    'license': 'Other proprietary'
+}
---
+++ src/models/__init__.py
@@ -0,0 +1,1 @@
+from . import spqm_installation
